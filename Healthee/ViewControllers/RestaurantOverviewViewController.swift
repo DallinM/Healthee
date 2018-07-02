9be6c7d2d0ccddbdf89887cddc77b0853eee92e3
Dallin <dallinmcconnell@gmail.com>
2018-07-02 16:55:29 -0600

    Added storyboard files, got references set up, set up tab bar controller.

---
+++ Healthee/ViewControllers/RestaurantOverviewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantOverviewViewController.swift
+//  Healthee
+//
+//  Created by Dallin McConnell on 6/29/18.
+//  Copyright © 2018 Dallin McConnell. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantOverviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
